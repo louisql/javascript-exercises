735a23dd8d7c84ef93d9bd39e07ef74bfdd23279
Louis <louis.queruau@gmail.com>
2022-06-04 14:24:52 -0400

    completed exo 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = '';
+    for (let i = 0; i < string.length; i++) {
+        newString += string[string.length - (i+1)]
+    };
+    return newString
 };
+
+
+let test = reverseString('test');
+
+console.log(test);
+
 
 // Do not edit below this line
 module.exports = reverseString;
