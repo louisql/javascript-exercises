5d2b57fccd14a54ee47db5534d79e065e41479fc
Louis <louis.queruau@gmail.com>
2022-06-04 18:08:43 -0400

    completed exo 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, arg) {
+    for (let arg = 1; arg < arguments.length; arg++){
+    let arr = arguments[arg];
+        for (let i = 0; i < array.length; i++){
+            if (array[i] === arr) array.splice(i, 1);
+        }
+    } 
+return array;
+}
 
-};
-
+let test = removeFromArray([1, 2, 3], "1", 3);
+console.log(test);
 // Do not edit below this line
 module.exports = removeFromArray;
