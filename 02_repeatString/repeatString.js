c813c05a9d60d8267c8c61de9d90b2e4e433e5c0
Louis <louis.queruau@gmail.com>
2022-06-04 12:32:45 -0400

    completed exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    result ='';
+    if (num >= 0){
+        for (let i = 0; i < num; i++){
+            result += string;
+        }
+        return result;
+    } else if (num < 0) return 'ERROR';
+}
 
-};
+function number (){
+    return Math.floor(Math.random() * 1000)
+}
 
 // Do not edit below this line
 module.exports = repeatString;
