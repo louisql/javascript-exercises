2671628a02b864306c13e315016f472593fb0ba4
Louis <louis.queruau@gmail.com>
2022-06-04 21:20:43 -0400

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let total = 0;
+    if (int1 < 0 || int2 < 0 || !Number.isInteger(int1) || !Number.isInteger(int2)) {
+        return 'ERROR'
+    }
+    else if (int1 <= int2){
+        for (let i = int1; i <= int2; i++){
+            total += i;
+        }
+    }
+    else if (int1 > int2){
+        for (let i = int2; i <= int1; i++){
+            total += i;
+        }
+    }
+    return total;
 
 };
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
